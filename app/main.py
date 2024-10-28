43ad30aa1bc9371f9a2f758ecb0df8ad380eac2b
Olha Yukhnenko <pyolh36@gmail.com>
2024-10-28 22:27:33 +0200

    solve_task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
+
+    def set_spouse(self, spouse_name):
+
+        if spouse_name is None:
+            return
+
+        spouse = Person.people.get(spouse_name)
+        if spouse:
+            if not hasattr(self, "wife") and not hasattr(spouse, "husband"):
+                self.wife = spouse
+                spouse.husband = self
+            elif not hasattr(self, "husband") and not hasattr(spouse, "wife"):
+                self.husband = spouse
+                spouse.wife = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = []
+    for person_data in people:
+
+        person = Person(person_data["name"], person_data["age"])
+        person_instances.append(person)
+
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+
+        if "wife" in person_data:
+            person.set_spouse(person_data["wife"])
+        elif "husband" in person_data:
+            person.set_spouse(person_data["husband"])
+
+    return person_instances
